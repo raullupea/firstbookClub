8ec3d0841deeed9bbd7d8551a5f5cfe1cea97993
Raul Lupea <raul.lupea88@gmail.com>
2024-07-02 21:44:43 +0000

    Add functionality to the reason section button(read more /read less)

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function showSidebar() {
     closeMenu.style.display = "none"; 
   }
   
+  // Read more section //
+
+function readMore(){
+    let dots = document.getElementById("dots");
+    let invisibleText = document.getElementById("invisible-text");
+    let button = document.getElementById("pressBtn");
+  
+    if(dots.style.display !="none"){
+      dots.style.display ="none";
+      invisibleText.style.display = "inline";
+      button.innerHTML ="Read Less";
+    }
+    else{
+      dots.style.display ="inline";
+      invisibleText.style.display = "none";
+      button.innerHTML = "Read More";
+    }
+  }

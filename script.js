c868f8ec3e5bb74469203c4f6ed6980b116b9c0a
Raul Lupea <raul.lupea88@gmail.com>
2024-07-02 21:32:01 +0000

    Add functionality to the menu side bar when screen changes to smaller sizes

---
+++ script.js
@@ -0,0 +1,12 @@
+// Side Menu //
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+  }
+  
+  function closeSidebar(){
+    const closeMenu = document.querySelector(".sidebar");
+    closeMenu.style.display = "none"; 
+  }
+  
